4b4efa1ded9856876611d88c57046dd2ee268817
RayhanSalman123 <raihansacramento@gmail.com>
2020-11-03 16:12:02 +0700

    [F-2][F-3] Memodifikasi Authentifikasi Login, Logout

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'usr_name' => 'Admin',
+            'usr_email' => 'admin@gmail.com',
+            'usr_password' => Hash::make('admin123'),
+            'usr_email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'usr_verification_token' => str_replace('/', '', Hash::make(Str::random(12))),
+            'usr_is_active' => true,
+        ]);
+
+        $admin->assignRole('admin');
+
+        $siswa = User::create([
+            'usr_name' => 'Mayang Sari',
+            'usr_email' => 'mayangsari12@gmail.com',
+            'usr_password' => Hash::make('mayangsari'),
+            'usr_email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'usr_verification_token' => str_replace('/', '', Hash::make(Str::random(12))),
+            'usr_is_active' => true,
+        ]);
+
+        $siswa->assignRole('student');
+
+        $guru = User::create([
+            'usr_name' => 'Nini Nurul',
+            'usr_email' => 'nini@gmail.com',
+            'usr_password' => Hash::make('nininurul'),
+            'usr_email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'usr_verification_token' => str_replace('/', '', Hash::make(Str::random(12))),
+            'usr_is_active' => true,
+        ]);
+
+        $guru->assignRole('teacher');
+
+        $head_master = User::create([
+            'usr_name' => 'Dedi Hidayat',
+            'usr_email' => 'dedihidayat@gmail.com',
+            'usr_password' => Hash::make('12345678'),
+            'usr_email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'usr_verification_token' => str_replace('/', '', Hash::make(Str::random(12))),
+            'usr_is_active' => true,
+        ]);
+
+        $head_master->assignRole('head_master');
+    }
+}
