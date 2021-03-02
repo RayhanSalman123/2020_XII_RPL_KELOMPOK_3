75a9939730f08d9cb5814e8a03ae20684ddacc49
mujahidmunir <mujahidmunir15@gmail.com>
2021-03-02 15:39:40 +0700

    .

--- resources/views/admin/schedule/list-schedule.blade.php
+++ resources/views/admin/schedule/list-schedule.blade.php
@@ -3,10 +3,11 @@
 @section('content')
     <div class="col-md-12">
 
-        @foreach($class as $dataClass)
+        @foreach ($class as $dataClass)
             <div class="card">
                 <div class="card-header">
-                    <h2 class="text-left">{{$dataClass->grade_name . ' ' . $dataClass->major_name  . '  ' . $dataClass->group}}</h2>
+                    <h2 class="text-left">
+                        {{ $dataClass->grade_name . ' ' . $dataClass->major_name . '  ' . $dataClass->group }}</h2>
                 </div>
                 <div class="card-body" style="font-size: 8px;">
                     <div class="table-responsive m-b-40">
@@ -15,30 +16,28 @@
                             <thead>
                             <tr>
 
-                                @foreach($day as $dataDay)
-                                    <th class="text-left" style="width: 18%">{{$dataDay->days_name}}</th>
+                                @foreach ($day as $dataDay)
+                                    <th class="text-left" style="width: 18%">{{ $dataDay->days_name }}</th>
                                 @endforeach
                             </tr>
                             </thead>
                             <tbody>
-
-                                <?php
-                                for ($i=1; $i<=10; $i++) {  ?>
-                                   <tr class="text-left">
-                                    <?php
-                                    for ($d=2; $d<=6; $d++) {  ?>
-                                        @forelse (\App\Helpers\Jadwal::MapelDay($dataClass->class_id,$d, $i) as $Mapel)
-                                            <td class="text-left">{{$Mapel->name_subject.' '.' - '.' '.$Mapel->usr_name . ' ' . '-' . ' ' . $Mapel->hour_notes}}</td>
-                                        @empty
-                                            <td class="text-left text-white" style="background-color: darkred">Kosong</td>
-                                        @endforelse
-
-
-                                        <?php  } ?>
-                                </tr>
-                                <?php  }  ?>
+                            <?php for ($i = 1; $i <= 10; $i++) { ?>
+                            <tr
+                                class="text-left">
+                                <?php for ($d = 2; $d <= 6; $d++) { ?> @forelse
+                                        (\App\Helpers\Jadwal::MapelDay($dataClass->class_id,$d, $i) as $Mapel)
+                                    <td class="text-left">
+                                        {{ $Mapel->name_subject . ' ' . ' - ' . ' ' . $Mapel->usr_name . ' ' . '-' . ' ' . $Mapel->hour_notes }}
+                                    </td>
+                                @empty
+                                    <td class="text-left text-white" style="background-color: darkred">Kosong</td>
+                                @endforelse
 
 
+                                <?php } ?>
+                            </tr>
+                            <?php } ?>
 
 
                             </tbody>
